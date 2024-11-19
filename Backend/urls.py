7f3e8e874f9cdc29ac92d54ff9a2c4c7010210ce
Ugrit Chaichana <ugrit.booth@gmail.com>
2024-11-19 08:42:19 +0700

    rename gold

--- Backend/urls.py
+++ Backend/urls.py
@@ -2,8 +2,6 @@ from django.urls import path,include
 from .views import test_gcp_connection_view
 
 urlpatterns = [
-    # path('th', include('Gold_TH.urls')),
-    # path('us/', include('Gold_US.urls')),
     path('currency/', include('currency.urls')),
     path('gold/', include('gold.urls')),
     path('api/test-gcp/', test_gcp_connection_view, name='test_gcp_connection'),
