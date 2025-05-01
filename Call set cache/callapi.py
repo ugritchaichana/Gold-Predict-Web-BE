07df1fc9034cd1117263091a1e8c21c8adc867ff
gartoon285 <gartoon285@gmail.com>
2025-05-01 23:43:19 +0700

    Update callapi.py

--- Call set cache/callapi.py
+++ Call set cache/callapi.py
@@ -2,19 +2,19 @@ import requests
 import datetime
 
 def call_api():
-    url = "https://api.coindesk.com/v1/bpi/currentprice.json"  # ตัวอย่าง API
+    url = "http://localhost:8000//Utility/set-cache"  # ตัวอย่าง API
     try:
-        response = requests.get(url, timeout=10)
+        response = requests.get(url, timeout=30)
         response.raise_for_status()  # ถ้ามี error จะ throw exception
         data = response.json()
 
         # ตัวอย่าง: บันทึกข้อมูลและเวลาไว้ในไฟล์
-        with open("/home/username/scripts/api_log.txt", "a") as f:
+        with open("/home/toonasboothas/Gold-Predict-Web-BE/Call set cache/api_log.txt", "a") as f:
             f.write(f"[{datetime.datetime.now()}] Response:\n")
             f.write(str(data) + "\n\n")
 
     except requests.RequestException as e:
-        with open("/home/username/scripts/api_log.txt", "a") as f:
+        with open("/home/toonasboothas/Gold-Predict-Web-BE/Call set cache/api_log.txt", "a") as f:
             f.write(f"[{datetime.datetime.now()}] ERROR: {e}\n\n")
 
 if __name__ == "__main__":
