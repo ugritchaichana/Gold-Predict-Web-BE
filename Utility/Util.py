a64cf102bef42c2a81410dfb9310c51f38dfd457
gartoon285 <gartoon285@gmail.com>
2025-05-01 16:48:23 +0700

    Update Util.py

--- Utility/Util.py
+++ Utility/Util.py
@@ -22,6 +22,8 @@ def set_cache(request):
                 for index_frame in range(len(gold_query_frame)):        
                     to_request.GET=QueryDict(f'db_choice={gold_query_choice[index_choice]}&max=100&frame={gold_query_frame[index_frame]}&cache=True')
                     get_gold_data(to_request)
+                    to_request.GET=QueryDict(f'db_choice={gold_query_choice[index_choice]}&max=100&frame={gold_query_frame[index_frame]}&cache=True&display=chart')
+                    get_gold_data(to_request)
             for index_frame in range(len(gold_query_frame)):
                 to_request.GET=QueryDict(f'frame={gold_query_frame[index_frame]}&cache=True&max=100&display=chart')
                 get_currency_data(to_request)
