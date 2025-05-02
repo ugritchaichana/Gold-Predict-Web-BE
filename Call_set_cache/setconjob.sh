6dd97c0e4ad7579c5bc45c28598788281c8610b7
gartoon285 <gartoon285@gmail.com>
2025-05-02 14:56:31 +0700

    Update setconjob.sh

--- Call_set_cache/setconjob.sh
+++ Call_set_cache/setconjob.sh
@@ -6,7 +6,7 @@ SCRIPT_PATH="/home/toonasboothas/Gold-Predict-Web-BE/Call_set_cache/call_api.py"
 LOG_PATH="/home/toonasboothas/Gold-Predict-Web-BE/Call_set_cache/log.txt"
 
 # สร้าง cron expression
-CRON_JOB="7 */2 * * * $PYTHON_PATH $SCRIPT_PATH >> $LOG_PATH 2>&1"
+CRON_JOB="*/7 * * * * $PYTHON_PATH $SCRIPT_PATH >> $LOG_PATH 2>&1"
 
 # ตรวจสอบว่ามี cron นี้อยู่แล้วหรือยัง
 (crontab -l 2>/dev/null | grep -F "$SCRIPT_PATH") > /dev/null
